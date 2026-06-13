61a0c563c5a0a80a5a5962eb80740ccad9db7521
fujitani sora <fujitanisora0414@gmail.com>
2026-06-13 19:06:01 +0900

    chore: stop committing vendor/ (already gitignored)
    vendor/ was force-added previously but is in .gitignore. Untrack it: builds
    use the module cache, and goreleaser's before-hook regenerates vendor/ at
    release time. Keeps the OpenCode wasm dependency (~16M) out of the repo.

--- vendor/github.com/ncruces/go-sqlite3/internal/util/pointer.go
+++
@@ -1,3 +0,0 @@
-package util
-
-type Pointer struct{ Value any }
--- vendor/github.com/ncruces/go-sqlite3/vfs/os_std_sync.go
+++
@@ -1,9 +0,0 @@
-//go:build !(linux || darwin) || sqlite3_flock
-
-package vfs
-
-import "os"
-
-func osSync(file *os.File, _ OpenFlag, _ SyncFlag) error {
-	return file.Sync()
-}
